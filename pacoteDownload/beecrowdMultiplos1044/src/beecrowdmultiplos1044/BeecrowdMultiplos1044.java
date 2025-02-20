1a7c17f521281ba00484fa2c9820e062a2663f41
Alexandre Paiva <alxd.paiva@outlook.com>
2025-02-19 21:27:23 -0300

    Correção no código.

--- pacoteDownload/beecrowdMultiplos1044/src/beecrowdmultiplos1044/BeecrowdMultiplos1044.java
+++ pacoteDownload/beecrowdMultiplos1044/src/beecrowdmultiplos1044/BeecrowdMultiplos1044.java
@@ -19,7 +19,7 @@ public class BeecrowdMultiplos1044 {
         Scanner teclado = new Scanner(System.in);
         int A = teclado.nextInt();
         int B = teclado.nextInt();
-        if (A % B == 0 && B % A == 0){
+        if (A % B == 0 || B % A == 0){
             System.out.println("Sao Multiplos");
         } else {
             System.out.println("Nao sao Multiplos");
