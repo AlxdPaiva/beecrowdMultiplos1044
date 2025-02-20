8f53963829e50307e6779e362f1c67845b3d1395
Alexandre Paiva <alxd.paiva@outlook.com>
2025-02-19 21:24:57 -0300

    Program added

---
+++ pacoteDownload/beecrowdMultiplos1044/src/beecrowdmultiplos1044/BeecrowdMultiplos1044.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package beecrowdmultiplos1044;
+import java.util.Scanner;
+
+/**
+ *
+ * @author alxdr
+ */
+public class BeecrowdMultiplos1044 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);
+        int A = teclado.nextInt();
+        int B = teclado.nextInt();
+        if (A % B == 0 && B % A == 0){
+            System.out.println("Sao Multiplos");
+        } else {
+            System.out.println("Nao sao Multiplos");
+        }
+    }
+    
+}
